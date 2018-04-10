528a5030522b9cdc32da2b21163ea811acbf90a8
Frank Pommerening <frank@pommerening-online.de>
2018-04-10 21:43:58 +0200

    Add cluster sample

---
+++ Talk-Microservice-RabbitMQ/Cluster/src/Contracts/ClusterMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FP.MdDevDays2018.Cluster.Contracts
+{
+    public class ClusterMessage
+    {
+        public string Message { get; set; }
+
+        public string Host { get; set; }
+
+        public DateTime Timestamep { get; set; }
+    }
+}
