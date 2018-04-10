c23f79e051ec3581911ffc5d822196b842f5955d
Frank Pommerening <frank@pommerening-online.de>
2018-04-10 21:40:29 +0200

    Add ssl sample

---
+++ Talk-Microservice-RabbitMQ/SSL/src/Contracts/MeteringValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FP.MdDevDays2018.IoTApp.Contracts
+{
+  
+        public class MeteringValue
+        {
+            public decimal Value { get; set; }
+
+            public string ObisCode { get; set; }
+
+            public DateTime Timestamp { get; set; }
+
+            public string Host { get; set; }
+        }
+   
+}
