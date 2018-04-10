18cc19fda0de0c39a48f216d262ac24f10d507df
Frank Pommerening <frank@pommerening-online.de>
2018-04-10 20:57:38 +0200

    Add basis samples

---
+++ Talk-Microservice-RabbitMQ/Basics/PublishSubscribe/StromVertrag.cs
@@ -0,0 +1,9 @@
+﻿namespace FP.MdDevDays2018.Basics.PublishSubscribe
+{
+    public class StromVertrag : IVertrag
+    {
+        public string Vertragsnummer { get; set; }
+
+        public string Zählernummer { get; set; }
+    }
+}
---
+++ Talk-Microservice-RabbitMQ/Basics/RequestResponse/MyResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace FP.MdDevDays2018.Basics.RequestResponse
+{
+    public class MyResponse
+    {
+        public int Sum { get; set; }
+    }
+}
